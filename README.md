777757fb5f7c542b077ea70e6f53421e5588dd5d
JoanAndoni <mcr_joan@hotmail.com>
2019-02-01 10:46:25 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Database for elections in DB2
+
+Homework for Bases de datos avanzadas in the Tecnológico de Monterrey CSF with the professor Vicente Cubells Nonell
+
+## Prerequisites
+
+- Install docker in ubuntu
+- Download the image of db2express
+
+## Installation
+
+Run the docker container with the following command
+
+```bash
+sudo docker run -it -p 50000:50000 -eDB2INST1_PASSWORD=db2inst1-pwd -e LICENSE=accept ibmcom/db2express-c:latest bash
+```
+Create an instance to log in db2
+
+```bash
+sudo su - db2inst1
+```
+After the creation of the instance log in db2
+
+```bash
+db2
+```
+
+## Usage
+
+Then run the scripts in the following order:
+
+* Tarea1-db2.sql
+* Tarea1-db2-fill.sql
+* Tarea1-db2-querys.sql
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+By
+* [Joan Andoni] (https://github.com/JoanAndoni)
+* [Alejandra Tubilla] (https://github.com/alejandratub)
---
+++ Tarea1-db2-querys.sql
@@ -0,0 +1,56 @@
+-- Joan Andoni
+-- Alejandra Tubilla
+
+-- Tarea 1
+-- QUERYS IN THE DATABASE in db2
+
+--INSERTS FOR THE UDATES AND DELETES
+INSERT INTO elecciones(id_elecciones, localidad, fecha, EStart, EEnd) VALUES( 5, 'Chetumal', '2017-06-25', '2018-06-10', '2024-06-10')
+INSERT INTO colegio(id_colegio, numeroElectores, id_elecciones, EStart, EEnd) VALUES(11, 1322, 5, '2018-06-10', '2018-06-30')
+INSERT INTO mesa(id_mesa, noMesa, id_colegio, EStart, EEnd) VALUES(21, 3, 11, '2018-06-10', '2018-06-30')
+INSERT INTO rol(rol, id_mesa, EStart, EEnd) VALUES('Presidente', 21, '2018-06-10', '2018-06-30')
+INSERT INTO partido(siglas, nombre, presidente, EStart, EEnd) VALUES('ABC','ABECE', 'Armando', '2018-06-10', '2024-06-10')
+INSERT INTO votante (IFE, nombre, fechaNacimiento, direccion, nacionalidad, id_mesa, id_partido, EStart, EEnd) VALUES ('A00569929', 'Rosa Cabarcas', '1920-05-10', 'Calle Cartagena 32', 'Mexico', 21, 'ABC', '2018-06-10', '2018-06-30')
+INSERT INTO apoderados(id_apoderado, nombre, id_partido, EStart, EEnd) VALUES(16, 'Regina Monroy','ABC', '2018-06-10', '2018-06-30')
+
+-- Apoderados
+UPDATE apoderados SET nombre='Serena Monroy' WHERE id_apoderado=16
+SELECT * FROM apoderados
+DELETE FROM apoderados WHERE id_apoderado=16 AND id_partido='ABC'
+SELECT * FROM hist_apoderados
+
+-- Votante
+UPDATE votante SET nombre='Delgadina La Niña', direccion='La que sea' WHERE IFE='A00569929'
+SELECT * FROM votante
+DELETE FROM votante WHERE nombre='Delgadina La Niña'
+SELECT * FROM hist_votante
+
+-- Partido
+UPDATE partido SET nombre='Americanos Unidos' WHERE siglas='ABC'
+SELECT * FROM partido
+DELETE FROM partido WHERE siglas='ABC'
+SELECT * FROM hist_partido
+
+-- Roles
+UPDATE rol SET rol='Vocal' WHERE rol='Presidente' AND id_mesa=21
+SELECT * FROM rol
+DELETE FROM rol WHERE rol='Vocal' AND id_mesa=21
+SELECT * FROM hist_rol
+
+-- Mesas
+UPDATE mesa SET noMesa = 1 WHERE id_mesa = 21
+SELECT * FROM mesa
+DELETE FROM mesa WHERE id_colegio=11 AND noMesa = 1
+SELECT * FROM hist_mesa
+
+-- Colegios
+UPDATE colegio SET numeroElectores=732 WHERE id_colegio=11
+SELECT * FROM colegio
+DELETE FROM colegio WHERE numeroElectores=732 AND id_colegio=11
+SELECT * FROM hist_colegio
+
+-- Elecciones
+UPDATE elecciones SET localidad='Cuernavaca' WHERE id_elecciones=1
+SELECT * FROM elecciones
+DELETE FROM elecciones WHERE localidad='Chetumal'
+SELECT * FROM hist_elecciones
